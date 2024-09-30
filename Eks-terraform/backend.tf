15bd38ee8b8cf5c4f3795073d5c2dd4a375c356f
eks-terraform <167340645+adityagithb@users.noreply.github.com>
2024-09-30 20:18:19 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "adibs" # Replace with your actual S3 bucket name
+    bucket = "adips" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
